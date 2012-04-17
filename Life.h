5ae014a49d7a08f8e2c1bea78ce705a8e415dc47
Geoffrey Parker <professional.cynic@gmail.com>
2012-04-17 18:21:10 -0500

    indentation cleanup, added a few comments

--- Life.h
+++ Life.h
@@ -5,52 +5,63 @@
 
 template <typename T>
 class Life {
-    private:
-      vector< vector<T> > grid;
-      void read(istream& in);
-    
-    public:
-      Life(istream& in);
-      void simulate(int numTurns);    
-      void takeTurn();
-      void print(ostream& out);
+ private:
+  vector< vector<T> > grid;
+  void read(istream& in);
+  
+ public:
+  Life(istream& in);
+  void simulate(int numTurns);    
+  void takeTurn();
+  void print(ostream& out);
 };
 
-// Abstract cell
 class AbstractCell {
-    private:
-      bool alive;
-      int numNeighbors;
-    
-    public:
-      virtual void countNeighbors() = 0;
-      virtual void turn() = 0;
+ private:
+  bool alive;
+  int numNeighbors;
+  
+ public:
+  virtual void countNeighbors() = 0;
+  virtual void turn() = 0;
 };
 
 class Cell {};
 
 class FredkinCell : AbstractCell {
-    private:
-      int age;
-    public:
-      FredkinCell() : age (0) {
-      }
+ private:
+  int age;
+ public:
+  FredkinCell();
 };
 
 class ConwayCell : AbstractCell {
-    
 };
 
+// ----
+// Life
+// ----
+
+/**
+ * Advance the board by one turn.
+ */
 void Life::takeTurn() {
-        for(int i = 0; i < grid.size(); i++)
-            for(int j = 0; j < grid[0].size(); j++)
-                grid[i][j].countNeighbors();
-        
-        for(int i = 0; i < grid.size(); i++)
-            for(int j = 0; j < grid[0].size(); j++)
-                grid[i][j].turn();
-    }
-    
+  for(int i = 0; i < grid.size(); i++)
+    for(int j = 0; j < grid[0].size(); j++)
+      grid[i][j].countNeighbors();
+  
+  for(int i = 0; i < grid.size(); i++)
+    for(int j = 0; j < grid[0].size(); j++)
+      grid[i][j].turn();
+}
+
+// -----------
+// Conway Cell
+// -----------
+
+/**
+ * Advance the cell state by one turn.
+ */
 void ConwayCell::turn() {
   if (numNeighbors == 3)
     alive = true;
@@ -58,10 +69,23 @@ void ConwayCell::turn() {
     alive = false;
 }
 
+// ------------
+// Fredkin Cell
+// ------------
+
+/**
+ * Construct a new Fredkin Cell with age 0.
+ */
+FredkinCell::FredkinCell() : age (0) {
+}
+
+/**
+ * Advance the cell state by one turn.
+ */
 void FredkinCell::turn() {
-    if (numNeighbors % 2 == 1)
-      alive = true;
-    else
-      alive = false;
-    age++;
+  if (numNeighbors % 2 == 1)
+    alive = true;
+  else
+    alive = false;
+  age++;
 }
