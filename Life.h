fddbe0f29a08fe91fdfd2abc217fd2985a827a2b
Geoffrey Parker <professional.cynic@gmail.com>
2012-04-24 17:39:25 -0500

    Filled out documentation

--- Life.h
+++ Life.h
@@ -5,7 +5,13 @@
 
 using namespace std;
 class Position {
-  friend const Position operator+(Position lhs, const Position& rhs) {
+  /**
+   * + operator for Position
+   * @param lhs the Position on the left hand side of the operator
+   * @param rhs the Position on the right hand side of the operator
+   * @return Position with lhr.r + rhs.r and lhs.c + rhs.c by value.
+   */
+  friend Position operator+(Position lhs, const Position& rhs) {
     return lhs += rhs;
   }
   
@@ -15,11 +21,7 @@ class Position {
   
   Position(int, int);
   
-  Position operator+=(const Position& rhs) {
-    r += rhs.r;
-    c += rhs.c;
-    return *this;
-  }
+  Position& operator+=(const Position& rhs);
 };
 
 const static Position NORTH(-1, 0);
@@ -61,16 +63,16 @@ class AbstractCell {
   
  public:
   bool alive;
-  virtual char name() = 0;
+  virtual char name() const = 0;
   virtual void turn() = 0;
-  virtual const vector<Position>& neighbors() = 0;
+  virtual const vector<Position>& neighbors() const = 0;
   
   virtual operator bool ();
   virtual operator int ();
   
   virtual void setNeighbors(int);
   
-  virtual AbstractCell* clone() = 0;
+  virtual AbstractCell* clone() const = 0;
 };
 
 class Cell {
@@ -83,8 +85,8 @@ class Cell {
   ~Cell();
   
   void turn();
-  const vector<Position>& neighbors();
-  char name();
+  const vector<Position>& neighbors() const;
+  char name() const;
   
   void setNeighbors(int);
   
@@ -97,10 +99,10 @@ class ConwayCell : public AbstractCell {
   ConwayCell(char);
   
   void turn();
-  const vector<Position>& neighbors();
-  char name();
+  const vector<Position>& neighbors() const;
+  char name() const;
   
-  AbstractCell* clone();
+  AbstractCell* clone() const;
 };
 
 class FredkinCell : public AbstractCell {
@@ -110,10 +112,10 @@ class FredkinCell : public AbstractCell {
   FredkinCell(char);
   
   void turn();
-  const vector<Position>& neighbors();
-  char name();
+  const vector<Position>& neighbors() const;
+  char name() const;
   
-  AbstractCell* clone();
+  AbstractCell* clone() const;
   operator int();
 };
 
@@ -121,13 +123,33 @@ class FredkinCell : public AbstractCell {
 // position
 // --------
 
+/**
+ * Two int constructor for position
+ * @param r the row value of the position
+ * @param c the col value of the position
+ */
 Position::Position(int r = 0, int c = 0) : r (r), c(c) {
+}
+
+/**
+ * The += operator of Position
+ * @param rhs The right hand side of the operator
+ * @return reference to *this
+ */
+Position& Position::operator +=(const Position& rhs) {
+  r += rhs.r;
+  c += rhs.c;
+  return *this;
 }
 
 // ----
 // Life
 // ----
 
+/**
+ * istream& Life Constructor
+ * @param in the istream& to read input from
+ */
 template <typename T>
 Life<T>::Life(istream& in) {
   generation = 0;
@@ -174,12 +196,20 @@ void Life<T>::takeTurn() {
   generation++;
 }
 
+/**
+ * Run the game the specified number of turns
+ * @param numTurns the number of turns to run for.
+ */
 template <typename T>
 void Life<T>::simulate(int numTurns) {
   while(numTurns-- > 0)
     takeTurn();
 }
 
+/**
+ * Print out the current state of the game
+ * @param out the ostream& to print out to.
+ */
 template <typename T>
 void Life<T>::print(ostream& out) {
   out << "Generation = " << generation << ", Population = " << population << "." << endl;
@@ -191,6 +221,10 @@ void Life<T>::print(ostream& out) {
   out << endl;
 }
 
+/**
+ * Count and set the number of alive neighbors of the specified cell.
+ * @param cell the cell to count the neighbors of
+ */
 template <typename T>
 void Life<T>::countNeighbors(T& cell) {
   vector<Position> adjacent = cell.neighbors();
@@ -201,7 +235,11 @@ void Life<T>::countNeighbors(T& cell) {
   cell.setNeighbors(numNeighbors);
 }
 
-
+/**
+ * see if the specified cell is alive
+ * @param adj a Position specifying the grid location of the cell to check relative to current location.
+ * @return bool representing if the cell is alive or not.
+ */
 template <typename T>
 bool Life<T>::isAlive(Position adj) {
   Position p = current + adj;
@@ -214,15 +252,27 @@ bool Life<T>::isAlive(Position adj) {
 // Abstract Cell
 // -------------
 
+/**
+ * Set the neighbor count to the given value
+ * @param n the value to set numNeighbors to.
+ */
 void AbstractCell::setNeighbors(int n) {
   if(n < 0 || n > 8) throw(invalid_argument("n must be between 0 and 8, inclusive"));
   numNeighbors = n;
 }
 
+/**
+ * Conversion to bool representing the aliveness of the cell.
+ * @return bool representing whether the cell is alive or not
+ */
 AbstractCell::operator bool() {
   return alive;
 }
 
+/**
+ * Conversion to int representing the age of the cell.  Defaults to 0.
+ * @return 0
+ */
 AbstractCell::operator int() {
   return 0;
 }
@@ -231,6 +281,10 @@ AbstractCell::operator int() {
 // Cell
 // ----
 
+/**
+ * char constructor for Cell.  Creates a new Conway or Fredkin cell depending on input.
+ * @param c char representing the type and state of the cell to be created.
+ */
 Cell::Cell(char c){
   if(c == '.'|| c == '*')
     ptr = new ConwayCell(c);
@@ -238,6 +292,10 @@ Cell::Cell(char c){
     ptr = new FredkinCell(c);
 }
 
+/**
+ * Copy constructor
+ * @param other the Cell to copy
+ */
 Cell::Cell(const Cell& other) {
   if (!other.ptr)
     ptr = 0;
@@ -245,15 +303,25 @@ Cell::Cell(const Cell& other) {
     ptr = other.ptr->clone();
 }
 
+/**
+ * Copy assignment
+ * @param other the Cell to copy
+ */
 Cell& Cell::operator = (Cell other) {
   swap(ptr, other.ptr);
   return *this;
 }
-  
+
+/**
+ * Destructor.  Deletes ptr.
+ */  
 Cell::~Cell() {
   delete ptr;
 }
 
+/**
+ * Advance the state of the Cell by one turn.  Assumes numNeighbors has been set
+ */
 void Cell::turn() {
   ptr->turn();
   if(*ptr == 2 && *ptr) {
@@ -262,18 +330,34 @@ void Cell::turn() {
   }
 }
 
-const vector<Position>& Cell::neighbors() {
+/**
+ * Get a Position vector representing the Positions to consider as neighbors.
+ * @return const vector<Position>& representing the relative Positions to consider as neighbors.
+ */
+const vector<Position>& Cell::neighbors() const {
   return ptr->neighbors();
 }
 
-char Cell::name(){
+/**
+ * Get the character representing the current state of the Cell
+ * @return char representing the state of the cell
+ */
+char Cell::name() const {
   return ptr->name();
 }
 
+/**
+ * set the neighbor count of the Cell
+ * @param n the value to set the neighbor count to.
+ */
 void Cell::setNeighbors(int n) {
   ptr->setNeighbors(n);
 }
 
+/**
+ * Conversion to bool
+ * return the conversion to bool of the contained AbstractCell
+ */
 Cell::operator bool(){
   return *ptr;
 }
@@ -282,6 +366,10 @@ Cell::operator bool(){
 // Conway Cell
 // -----------
 
+/**
+ * char constructor for ConwayCell
+ * @param char representing the initial state of the cell.
+ */
 ConwayCell::ConwayCell(char c) {
   if (c == '.')
     alive = false;
@@ -301,18 +389,29 @@ void ConwayCell::turn() {
     alive = alive; //do nothing
 }
 
-const vector<Position>& ConwayCell::neighbors() {
+/**
+ * Get a Position vector representing the Positions to consider as neighbors.
+ * @return const vector<Position>& representing the relative Positions to consider as neighbors.
+ */
+const vector<Position>& ConwayCell::neighbors() const {
   return conwayNeighbors;
 }
 
-
-char ConwayCell::name() {
+/**
+ * Get the character representing the current state of the Cell
+ * @return char representing the state of the cell
+ */
+char ConwayCell::name() const {
   if(alive)
     return '*';
   return '.';
 }
 
-AbstractCell* ConwayCell::clone(){
+/**
+ * Create a clone of the cell
+ * @return pointer to the new cell.
+ */
+AbstractCell* ConwayCell::clone() const {
   return new ConwayCell(*this);
 }
 
@@ -321,6 +420,10 @@ AbstractCell* ConwayCell::clone(){
 // Fredkin Cell
 // ------------
 
+/**
+ * char constructor for FredkinCell
+ * @param char representing the initial state of the cell.
+ */
 FredkinCell::FredkinCell(char c) {
   age = 0;
   if (c == '-')
@@ -345,16 +448,18 @@ void FredkinCell::turn() {
 }
 
 /**
- * Tell which of the 8 surrounding cells are counted as neighbors. 
+ * Get a Position vector representing the Positions to consider as neighbors.
+ * @return const vector<Position>& representing the relative Positions to consider as neighbors.
  */
-const vector<Position>& FredkinCell::neighbors() {
+const vector<Position>& FredkinCell::neighbors() const {
   return fredkinNeighbors;
 }
 
 /**
- * 
+ * Get the character representing the current state of the Cell
+ * @return char representing the state of the cell
  */
-char FredkinCell::name() {
+char FredkinCell::name() const {
   if(!alive)
     return '-';
   if(age > 9)
@@ -362,10 +467,18 @@ char FredkinCell::name() {
   return (char) (age + 0x30);
 }
 
+/**
+ * Conversion to int.
+ * @return the age of the cell.
+ */
 FredkinCell::operator int() {
   return age;
 }
 
-AbstractCell* FredkinCell::clone(){
+/**
+ * Create a clone of the cell
+ * @return pointer to the new cell.
+ */
+AbstractCell* FredkinCell::clone() const {
   return new FredkinCell(*this);
 }
